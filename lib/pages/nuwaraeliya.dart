90b1fb9bb8b5997c3ad4203985fa71e2a7b04503
KokilaBandara <134389711+KokilaBandara@users.noreply.github.com>
2024-05-03 23:00:06 +0530

    nuwaraeliya_cord

--- lib/pages/nuwaraeliya.dart
+++ lib/pages/nuwaraeliya.dart
@@ -23,7 +23,7 @@ class NuwaraEliya extends StatelessWidget {
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
                 child: Image.asset(
-                  'lib/images/destination_page/n2.jpg', // Path to your image asset
+                  'lib/images/destinations_page/n2.jpg', // Path to your image asset
                 ),
               ),
             ),
