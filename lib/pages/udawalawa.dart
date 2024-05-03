3afc72c96cc786045d5d39d49226b82490f11829
KokilaBandara <134389711+KokilaBandara@users.noreply.github.com>
2024-05-03 16:42:27 +0530

    udawalawa_cord_edit

--- lib/pages/udawalawa.dart
+++ lib/pages/udawalawa.dart
@@ -23,7 +23,7 @@ class Udawalawa extends StatelessWidget {
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
                 child: Image.asset(
-                  'assets/pi2.jpg', // Path to your image asset
+                  'lib/images/destination_page/pi2.jpg', // Path to your image asset
                 ),
               ),
             ),
