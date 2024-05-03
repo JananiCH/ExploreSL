9bfd47758c3c57d8cff1c9ebb866a3db003d5ae6
KokilaBandara <134389711+KokilaBandara@users.noreply.github.com>
2024-05-03 16:45:43 +0530

    sigiriya_cord_edit

--- lib/pages/sigiriya.dart
+++ lib/pages/sigiriya.dart
@@ -23,7 +23,7 @@ class Sigiriya extends StatelessWidget {
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
                 child: Image.asset(
-                  'assets/s2.jpg', // Path to your image asset
+                  'lib/images/destination_page/s2.jpg', // Path to your image asset
                 ),
               ),
             ),
