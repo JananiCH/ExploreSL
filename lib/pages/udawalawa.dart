3de19c09187f824a632c89152792e8e456792278
KokilaBandara <134389711+KokilaBandara@users.noreply.github.com>
2024-05-10 01:39:18 +0530

    udawalawa_changed

--- lib/pages/udawalawa.dart
+++ lib/pages/udawalawa.dart
@@ -9,41 +9,27 @@ class Udawalawa extends StatelessWidget {
       appBar: AppBar(
         title: const Flexible(
           child: Text(
-            'Pinnawala',
+            'Udawalawa',
             overflow: TextOverflow.ellipsis, // Handles overflow by ellipsis
           ),
         ),
       ),
-      body: Stack(
+      body: Column(
         children: [
-          SizedBox.expand(
-            child: Transform.scale(
-              scale: 360 / MediaQuery.of(context).size.width,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: Image.asset(
-                  'lib/images/pictures_d/pi2.jpg', // Path to your image asset
-                ),
-              ),
-            ),
+          Image.asset(
+            'lib/images/pictures_d/pi2.jpg', // Path to your image asset
           ),
-          const Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Add your content here
-                  Text(
-                    'Pinnawala is a locality in Rambukkana town in Kegalle district of Sri Lanka. It is located about 90 kilometers from Colombo city. Pinnawala is one of the main tourist areas in Sri Lanka.The Pinnawala Elephant Orphanage, the only one in the world, is also home to Sri Lanka s largest and only open- air zoo.',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Add your content here
+                Text(
+                  'Pinnawala is a locality in Rambukkana town in Kegalle district of Sri Lanka. It is located about 90 kilometers from Colombo city. Pinnawala is one of the main tourist areas in Sri Lanka.The Pinnawala Elephant Orphanage, the only one in the world, is also home to Sri Lanka s largest and only open- air zoo.',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
             ),
           ),
         ],
