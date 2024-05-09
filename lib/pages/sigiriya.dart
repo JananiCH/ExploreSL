7cd2cc2ea99b79676df6a65827a71282993f161f
KokilaBandara <134389711+KokilaBandara@users.noreply.github.com>
2024-05-10 01:34:49 +0530

    sigiriya_edited

--- lib/pages/sigiriya.dart
+++ lib/pages/sigiriya.dart
@@ -14,36 +14,22 @@ class Sigiriya extends StatelessWidget {
           ),
         ),
       ),
-      body: Stack(
+      body: Column(
         children: [
-          SizedBox.expand(
-            child: Transform.scale(
-              scale: 360 / MediaQuery.of(context).size.width,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: Image.asset(
-                  'lib/images/pictures_d/s2.jpg', // Path to your image asset
-                ),
-              ),
-            ),
+          Image.asset(
+            'lib/images/pictures_d/s2.jpg', // Path to your image asset
           ),
-          const Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Add your content here
-                  Text(
-                    'Sigiriya is translated from Sinhalese as the “ Lion Rock ” (Sinha-giriya). This site of historical and archaeological significance is listed in the UNESCO s World Heritage List and is also called the eighth wonder of the world.',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Add your content here
+                Text(
+                  'Sigiriya is translated from Sinhalese as the “ Lion Rock ” (Sinha-giriya). This site of historical and archaeological significance is listed in the UNESCO s World Heritage List and is also called the eighth wonder of the world. ',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
             ),
           ),
         ],
