ee2b1b9f5cb59e1a118dac6fbfb5eee510381ac4
KokilaBandara <134389711+KokilaBandara@users.noreply.github.com>
2024-05-10 01:03:06 +0530

    colombo_edit

--- lib/pages/colombo.dart
+++ lib/pages/colombo.dart
@@ -14,36 +14,22 @@ class colombo extends StatelessWidget {
           ),
         ),
       ),
-      body: Stack(
+      body: Column(
         children: [
-          SizedBox.expand(
-            child: Transform.scale(
-              scale: 380 / MediaQuery.of(context).size.width,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: Image.asset(
-                  'lib/images/pictures_d/c2.jpg', // Path to your image asset
-                ),
-              ),
-            ),
+          Image.asset(
+            'lib/images/pictures_d/c2.jpg', // Path to your image asset
           ),
-          const Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Add your content here
-                  Text(
-                    'Colombo is the largest city and commercial capital of Sri Lanka. Sri Jayawardenepura Kotte, the administrative capital, is located on the outskirts of Colombo. Colombo is the administrative center of Colombo District. A population of 752,993 lives within the city limits of Colombo. ',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Add your content here
+                Text(
+                  'Colombo is the largest city and commercial capital of Sri Lanka. Sri Jayawardenepura Kotte, the administrative capital, is located on the outskirts of Colombo. Colombo is the administrative center of Colombo District. A population of 752,993 lives within the city limits of Colombo. ',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
             ),
           ),
         ],
