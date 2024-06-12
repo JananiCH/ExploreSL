42c000d4f27ddcdb1ea55aa1b2fa3b85d4e46948
JananiCH <hewawitharanajanani@gmail.com>
2024-06-12 20:56:57 +0530

    guide

--- lib/pages/GuideProfile.dart
+++ lib/pages/GuideProfile.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:exploresl_login/models/guide_model.dart';
 import 'package:exploresl_login/pages/HomePage.dart';
