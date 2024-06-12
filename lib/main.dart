fbd54f05926b0997b6aeff813b4cf979fd5e19c3
JananiCH <hewawitharanajanani@gmail.com>
2024-06-12 20:53:03 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:exploresl_login/pages/login.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
-import 'pages/HomePage.dart';
-import 'pages/guideSignUp.dart';
-import 'pages/testNavigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   const MyApp({super.key, Key? key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(home: HomeScreen());
+    return const MaterialApp(home: HomeScreen());
   }
 }
