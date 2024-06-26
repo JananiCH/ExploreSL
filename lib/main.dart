e62b07bd69e3acb90a1fc742135795ba03d28b8b
HiranMaleesha <maleesha20010527@gmail.com>
2024-06-26 11:53:38 +0530

    debugremover

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   MaterialApp(home: SplashScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen());
   }
 }
